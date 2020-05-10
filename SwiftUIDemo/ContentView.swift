8aaf85811e48fa89ecc0a21256f789aa340ef27d
perry14 <yuezuofang@vip.qq.com>
2020-05-10 22:02:19 +0800

    FaceID Demo.

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -7,37 +7,46 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
-    var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row) C\(col)")
-        }
-    }
-}
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
+    @State private var isUnlocked = false
 
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
-    }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
 
-    var body: some View {
-        VStack {
-            ForEach(0 ..< rows) { row in
-                HStack {
-                    ForEach(0 ..< self.columns) { column in
-                        self.content(row, column)
+        // 检查是否可以进行生物特征识别
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 如果可以，执行识别
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // 鉴权完成
+                DispatchQueue.main.async {
+                    if success {
+                        // 鉴权成功
+                        self.isUnlocked = true
+                    } else {
+                        // 鉴权失败
                     }
                 }
             }
+        } else {
+            // 没有生物指纹识别功能
         }
+    }
+    
+    var body: some View {
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
     }
 }
 
